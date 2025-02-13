4dad141722d6b1a959bad086c8f09eb973feb5d3
rizalsuryana <rizal.suryn@gmail.com>
2025-02-13 19:28:57 +0700

    product

--- Requests/request.ts
+++ Requests/request.ts
@@ -2,3 +2,8 @@ export async function getAllCategory() {
     const categoryRes = await fetch('https://fakestoreapi.com/products/categories');
     return categoryRes.json();
 }
+
+export async function getAllProducts() {
+    const productsRes = await fetch('https://fakestoreapi.com/products');
+    return productsRes.json();
+}
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import Category from './Category'
+import AllProducts from './AllProducts'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <Category />
+        <AllProducts />
     </div>
   )
 }
