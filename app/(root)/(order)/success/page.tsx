48a1cc2e9b67610bfaefef766f7636482f25d47f
rizalsuryana <rizal.suryn@gmail.com>
2025-02-15 13:58:17 +0700

    I'll develop this app to the better version

--- app/(root)/(order)/success/page.tsx
+++ app/(root)/(order)/success/page.tsx
@@ -1,8 +1,19 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const SuccessPage = () => {
   return (
-    <div>SuccessPage</div>
+    <div className='w-full h-[60vh] flex items-center justify-center flex-col'>
+      <Image src='/images/order.svg' alt='success' width={300} height={300}/>
+      <h1 className='mb-8 mt-2 text-3xl font-bold uppercase text-green-600'>
+      Order Successful
+      </h1>
+      <Link href='/'>
+        <Button >Back to Home</Button>
+      </Link>
+    </div>
   );
 };
 
